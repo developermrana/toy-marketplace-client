b61af0eab0d570d9052f642c08d7d877763e211c
mrana565 <rana565m@gmail.com>
2023-06-07 10:06:29 +0600

    impliment search all toys page

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,9 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 import ToyRow from "../AllToys/ToyRow";
 import PageTitle from "../PageTitle/PageTitle";
+import { useState } from "react";
 
 const AllToys = () => {
   const loadedToys = useLoaderData();
+  const [search, setSearch] = useState("");
 
   let toys;
 
@@ -18,7 +20,14 @@ const AllToys = () => {
       <h2 className="text-center text-[#405a7f] text-2xl font-bold py-10">
         All Toys
       </h2>
-
+      <form className="text-center">
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="search toy name"
+          className="input input-bordered input-secondary w-full max-w-xs mb-5"
+        />
+      </form>
       <div className="overflow-x-auto">
         <table className="table mx-auto">
           {/* head */}
@@ -33,9 +42,15 @@ const AllToys = () => {
             </tr>
           </thead>
           <tbody>
-            {toys.map((toy) => (
-              <ToyRow key={toy._id} toy={toy} />
-            ))}
+            {toys
+              .filter((toy) => {
+                return search.toLowerCase() === ""
+                  ? toy
+                  : toy.toy_name.toLowerCase().includes(search);
+              })
+              .map((toy) => (
+                <ToyRow key={toy._id} toy={toy} />
+              ))}
           </tbody>
         </table>
       </div>
